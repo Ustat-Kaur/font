1f97748a545f88ad8e01a2bf121d45bb2d3d18f5
Ustat-Kaur <81715255+Ustat-Kaur@users.noreply.github.com>
2022-07-07 19:42:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWrist = 0;
+rightWrist = 0;
+difference = 0; 
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,9 +17,19 @@ function modelLoaded(){
     console.log("poseNet is initialized!");
 }
 
-function gosPoses(results){
+function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        leftWrist = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist - rightWrist);
     }
+}
+function draw(){
+    background("#528560");
+    textSize(difference);
+    fill("#685285");
+    stroke("#685285");
+    text("ustat kaur", 50, 200);
 }
